743c03ba186b17f0b7bfc5aa3cdf627b3d1e0b56
div-antk <51283468+owaii@users.noreply.github.com>
2022-05-12 23:06:01 +0900

    Typoの修正とコンポーネントの追加

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,9 +13,9 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update() {
        if (Input.GetKey(KeyCode.LeftArrow)) {
-           transform.position += transform.foward * 0.1f;
+           transform.position += transform.forward * 0.1f;
        } else if (Input.GetKey(KeyCode.RightArrow)) {
-           transform.position -= transform.foward * 0.1f;
+           transform.position -= transform.forward * 0.1f;
        }
     }
 }
