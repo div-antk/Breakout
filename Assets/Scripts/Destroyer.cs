4d529b0fe4f6a732f1f308ebad041bbcf44dc450
div-antk <51283468+owaii@users.noreply.github.com>
2022-05-18 22:10:06 +0900

    当たった際の処理としてcollisionを記入

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 当たり判定が何かしらあったときに呼ばれるイベント関数
+    // 当たってきたオブジェクト情報が collision に代入される
+    private void OnCollisionEnter(Collision collision) {
+        Destroy(gameObject);
+    }
+}
