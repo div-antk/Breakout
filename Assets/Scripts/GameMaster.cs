281f5fa82fdb8b8f51ff05f29f2d4d2faccf208d
div-antk <51283468+owaii@users.noreply.github.com>
2022-05-20 21:39:23 +0900

    進行管理するスクリプトを作成してアタッチ

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    public int boxNum;
+    public float nowTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowTime += Time.deltaTime;
+        if (boxNum <= 0) {
+            GameOver();
+        }
+    }
+
+    public void GameOver() {
+        // Result というシーンに移動する
+        SceneManager.LoadScene("Result");
+    }
+}
